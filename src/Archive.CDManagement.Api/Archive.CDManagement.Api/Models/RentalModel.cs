fd5344962e51c9cb803956228eea8a83a58597f9
RGAUGER\Gauger <Rgauger.dev@gmail.com>
2020-09-17 11:35:37 +1000

    Setup repos, models and controllers for entities.

---
+++ src/Archive.CDManagement.Api/Archive.CDManagement.Api/Models/RentalModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Archive.CDManagement.Api.Models
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RentalModel : ControllerBase
+    {
+        public IEnumerable<RentalItemModel> RentalItems { get; set; }
+    }
+}
---
+++ src/Archive.CDManagement.Api/Archive.CDManagement.Api/Repositories/Abstractions/IStaffRepoitory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Archive.CDManagement.Api.Repositories.Abstractions
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IStaffRepoitory : ControllerBase
+    {
+    }
+}
